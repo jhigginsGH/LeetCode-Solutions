51d6c10ddf8ed93369b6a2e77d0eee3582de712e
jhiggins <jackson.higgins@icloud.com>
2025-02-28 07:43:42 -0500

    added comments

--- Strings/FindNeedle.java
+++ Strings/FindNeedle.java
@@ -1,7 +1,12 @@
+/**
+ * Question:
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack
+ */
+
 class Solution {
     public int strStr(String haystack, String needle) {
         if (haystack.contains(needle)){
-            return haystack.indexOf(needle);
+            return haystack.indexOf(needle); // Returns first index of the substring
         }
         else{
             return -1;
